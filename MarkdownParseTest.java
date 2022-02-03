ee4b0eb576f7334b778be971247968c99a51c715
KristinShuyiHan <97652556+KristinShuyiHan@users.noreply.github.com>
2022-02-03 10:24:49 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,7 +8,7 @@ import java.nio.file.Path;
 public class MarkdownParseTest {
     @Test
     public void addition() {
-        assertEquals(2, 1 + 0);
+        assertEquals(2, 1);
     }
     @Test
     public void testTestfile() throws IOException {
